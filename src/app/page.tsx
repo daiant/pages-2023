ef3881ed86fd390e7e842e38bb5c5f6b1b9b2c21
daiant <cmg2512@gmail.com>
2023-02-11 17:42:50 +0100

    actually scratch that

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,59 @@
 'use client';
 
 import Bubble from '@/components/Bubble/bubble';
-import { Montserrat } from '@next/font/google'
+import Nav from '@/components/Navigation/nav';
 import styles from './page.module.scss';
 
 
-const montse = Montserrat({ subsets: ['latin'], display: 'swap' })
 const bubbles: Array<Bubble> = [
   { title: 'me', img: 'https://github.com/daiant.png' },
   { title: 'Drag me' }
 ];
 function scrollIntoContent() {
-  document.getElementById('scroll-next')?.scrollIntoView({behavior: 'smooth', block: 'start'});
+  document.getElementById('scroll-next')?.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
 export default function Home() {
   const devMode: boolean = false;
   return (
-    <main className={`${styles.main} ${montse.className}`}>
+    <main className={`${styles.main}`}>
       <section id='splash' className={`${devMode ? styles.disableAnimations : ''} ${styles.front}`}>
         <div className={styles.title}>
           <h1>Carlos</h1>
           <h2>Frontend developer</h2>
           <h3>CSS magician</h3>
         </div>
-        <div className={styles.bubbles}>
-          {/* {bubbles.map((bubble: Bubble) => <Bubble key={bubble.title} title={bubble.title} img={bubble.img}></Bubble>)} */}
-        </div>
-        <div className={styles.scrollDown} onClick={scrollIntoContent}>
-          <span>See my</span>
-          <img src="/scroll.png" title='scroll down' />
-          <span>work</span>
-        </div>
+      </section>
+      <section className="monsteri">
+        
       </section>
       <div className={styles.mainPage}>
+        <Nav></Nav>
 
-      <nav className={styles.nav}>
-        <p>Carlos is</p>
-        <span>Burgir</span>
-      </nav>
-      <section id='scroll-next'>
-        <div className={styles.info}>
-          <h1>a CSS magician</h1>
-          <div className={styles.content}>
-            <p className={styles.jobDescription}>Java Fullstack developer at Laberit Sistemas</p>
-            <p className={styles.jobAdditionalInfo}>From 2020 to 2022 in Valencia, spain.</p>
-            <div className={styles.block}></div>
+        <section id='scroll-next'>
+          <div className={styles.info}>
+            <h1>a CSS magician</h1>
+            <div className={styles.content}>
+              <p className={styles.jobDescription}>Java Fullstack developer at Laberit Sistemas</p>
+              <p className={styles.jobAdditionalInfo}>From 2020 to 2022 in Valencia, spain.</p>
+              <div className={styles.block}></div>
+            </div>
+            <div className={styles.content}>
+              <p className={styles.jobDescription}>Java Fullstack developer at Laberit Sistemas</p>
+              <p className={styles.jobAdditionalInfo}>From 2020 to 2022 in Valencia, spain.</p>
+              <div className={styles.block}></div>
+            </div>
+            <div className={styles.content}>
+              <p className={styles.jobDescription}>Java Fullstack developer at Laberit Sistemas</p>
+              <p className={styles.jobAdditionalInfo}>From 2020 to 2022 in Valencia, spain.</p>
+              <div className={styles.block}></div>
+            </div>
           </div>
-          <div className={styles.content}>
-            <p className={styles.jobDescription}>Java Fullstack developer at Laberit Sistemas</p>
-            <p className={styles.jobAdditionalInfo}>From 2020 to 2022 in Valencia, spain.</p>
-            <div className={styles.block}></div>
+        </section>
+        <section>
+          <div className={styles.info}>
+            <h1>a Hobbyist</h1>
           </div>
-          <div className={styles.content}>
-            <p className={styles.jobDescription}>Java Fullstack developer at Laberit Sistemas</p>
-            <p className={styles.jobAdditionalInfo}>From 2020 to 2022 in Valencia, spain.</p>
-            <div className={styles.block}></div>
-          </div>
-        </div>
-      </section>
-      <section>
-        <div className={styles.info}>
-          <h1>a Hobbyist</h1>
-        </div>
-      </section>
+        </section>
       </div>
     </main>
   )
---
+++ src/components/Navigation/nav.tsx
@@ -0,0 +1,11 @@
+import styles from './nav.module.scss';
+
+export default function Nav() {
+    return (
+        <nav className={styles.nav}>
+            <p>Carlos is</p>
+            <img src="/burgir.svg" />
+        </nav>
+
+    )
+}
